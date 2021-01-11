2b5aedec4ad1d9369afb09445d06080a03ac5a14
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-01-11 19:17:22 +0200

    Bug fixes

--- src/format/utils/formatPercentage.ts
+++ src/format/utils/formatPercentage.ts
@@ -8,7 +8,7 @@ export const formatPercentage = (
 ) => {
     const delta = headPercentage - basePercentage;
 
-    return `${parseFloat(headPercentage.toFixed(2))}% ${
+    return `${Number(headPercentage.toFixed(2))}% ${
         Math.abs(delta) > APPROXIMATION_THRESHOLD
             ? formatPercentageDelta(delta)
             : ''
