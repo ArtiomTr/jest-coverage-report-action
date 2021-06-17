1bc30ca44e1e823e5275265e74a01df7f45e4b58
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-06-17 20:22:10 +0300

    Fixed documentation deployment script

--- docs/scripts/deploy.js
+++ docs/scripts/deploy.js
@@ -1,7 +1,11 @@
 const ghpages = require('gh-pages');
 
-ghpages.publish('out', {
-    branch: 'gh-pages',
-    message: `Deploying project ${new Date()}`,
-    dotfiles: true,
-});
+ghpages.publish(
+    'out',
+    {
+        branch: 'gh-pages',
+        message: `Deploying project ${new Date()}`,
+        dotfiles: true,
+    },
+    console.error
+);
