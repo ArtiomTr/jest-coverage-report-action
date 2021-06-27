5152836e89f90bac71cb081d07533645aa5eb822
Artiom Tretjakovas <44021713+ArtiomTr@users.noreply.github.com>
2021-06-27 11:38:28 +0300

    Removed ci_job_number from documentation index.tsx

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -36,8 +36,6 @@ on:
 jobs:
     coverage:
         runs-on: ubuntu-latest
-        env:
-            CI_JOB_NUMBER: 1
         steps:
             - uses: actions/checkout@v1
             - uses: artiomtr/jest-coverage-report-action@v2
