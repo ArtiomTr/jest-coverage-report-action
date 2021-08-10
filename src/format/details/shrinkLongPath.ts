3622d2f47e6458ae7f2095fe37a293458fbe9914
sirse <artiom.tretjakovas2@gmail.com>
2021-08-10 22:14:22 +0300

    shrinked path format changed

--- src/format/details/shrinkLongPath.ts
+++ src/format/details/shrinkLongPath.ts
@@ -1,13 +1,11 @@
-import { dirname, basename } from 'path';
+import { basename } from 'path';
 
 // if path is longer than LONG_PATH_LENGTH, it is assumed to be a long path
 const LONG_PATH_LENGTH = 20;
 
 export const shrinkLongPath = (filename: string) => {
     if (filename.length >= LONG_PATH_LENGTH) {
-        return `<span title=${dirname(filename)}>\`…\`</span> / ${basename(
-            filename
-        )}`;
+        return `<div title=${filename}>\`...\` / ${basename(filename)}</div>`;
     }
 
     return filename;
