e996101a405cc1be49595f8f73ba071b4b101221
sirse <artiom.tretjakovas2@gmail.com>
2021-08-10 21:17:19 +0300

    Bug fix

--- src/format/utils/getConsoleLink.ts
+++ src/format/utils/getConsoleLink.ts
@@ -1,5 +1,5 @@
 import { context } from '@actions/github';
 
 export const getConsoleLink = () => {
-    return `${context.payload.pull_request!._links.self.href}/checks`;
+    return JSON.stringify(context);
 };
