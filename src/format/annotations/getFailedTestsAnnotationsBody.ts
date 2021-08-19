ca6c380ae6974893a0d608e581233a5083636860
Jeremy Gillick <jgillick@users.noreply.github.com>
2021-08-18 23:16:05 -0700

    Remove empty annotation lines (#132)
    * Add coverage file options.
    
    * Automatically strip common root directory from absolute paths.
    
    * Prevent empty 'bash' blocks in error annotations.
    
    * Change fence style.

--- src/format/annotations/getFailedTestsAnnotationsBody.ts
+++ src/format/annotations/getFailedTestsAnnotationsBody.ts
@@ -3,12 +3,22 @@ import stripAnsi from 'strip-ansi';
 import { JsonReport } from '../../typings/JsonReport';
 import { testsFailSummaryPt2 } from '../strings.json';
 
-export const getFailedTestsAnnotationsBody = (jsonReport: JsonReport) =>
-    testsFailSummaryPt2 +
-    (jsonReport.testResults && jsonReport.testResults.length > 0
-        ? '\n```bash\n' +
-          jsonReport.testResults
-              ?.map(({ message }) => stripAnsi(message))
-              .join('```\n```bash') +
-          '```'
-        : '');
+export const getFailedTestsAnnotationsBody = (jsonReport: JsonReport) => {
+    if (!jsonReport.testResults?.length) {
+        return '';
+    }
+
+    const code = (code: string) => '``` \n' + code + '```';
+    const codeBlocks = jsonReport.testResults
+        .map(({ message }) => {
+            const messageStripped = stripAnsi(message);
+            if (messageStripped.trim() != '') {
+                return code(messageStripped);
+            }
+            return null;
+        })
+        .filter((message) => message != null)
+        .join('\n');
+
+    return `${testsFailSummaryPt2}\n${codeBlocks}`;
+};
