22185c14e39dd784ac0cea8694bad782573dead3
Sidharth Vinod <sidharthv96@gmail.com>
2021-08-29 01:27:05 +0530

    feat: ESBuild

---
+++ esbuild.cjs
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { build } = require('esbuild');
+build({
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/index.js',
+    loader: {
+        '.md': 'text',
+    },
+}).catch(() => process.exit(1));
