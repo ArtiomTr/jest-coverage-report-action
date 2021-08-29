148a4bcee0faa48e17f0d42489789a109ee76e75
Sidharth Vinod <sidharthv96@gmail.com>
2021-08-29 23:26:33 +0530

    update cache version

--- esbuild.cjs
+++ esbuild.cjs
@@ -2,7 +2,7 @@
 const { build } = require('esbuild');
 build({
     bundle: true,
-    minify: false,
+    minify: false, // Cannot minify due to https://github.com/node-fetch/node-fetch/issues/784
     platform: 'node',
     target: 'es2019',
     entryPoints: ['src/index.ts'],
