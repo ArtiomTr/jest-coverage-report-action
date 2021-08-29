c522414eb08350dd2d7c26fabcdf1acc03440a62
sirse <artiom.tretjakovas2@gmail.com>
2021-08-29 16:47:50 +0300

    Removed unfinished test

--- tests/stages/getCoverage.test.ts
+++
@@ -1,9 +0,0 @@
-import { getCoverage } from '../../src/stages/getCoverage';
-import { createDataCollector } from '../../src/utils/DataCollector';
-
-describe('getCoverage', () => {
-    it('default case', async () => {
-        const dataCollector = createDataCollector();
-        await getCoverage();
-    });
-});
