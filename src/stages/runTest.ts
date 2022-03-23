e81ab9b0b248f5fe1b4b3749d49fd2b34e74a1aa
therynamo <theryn.groetken@gemini.com>
2022-03-23 15:45:11 -0500

    feat: Improve Error Reporting for Coverage Threshold Failures

--- src/stages/runTest.ts
+++ src/stages/runTest.ts
@@ -1,16 +1,36 @@
 import { exec } from '@actions/exec';
 
+import { getErrorMessage, toErrorWithMessage } from '../utils/getErrorMessage';
 import { getTestCommand } from '../utils/getTestCommand';
+
+const THRESHOLD_NOT_MET_REGEX =
+    /Jest:.*coverage threshold for.*not met:.*%\S/gm;
 
 export const runTest = async (
     testCommand: string,
     workingDirectory?: string
 ) => {
-    await exec(
-        await getTestCommand(testCommand, 'report.json', workingDirectory),
-        [],
-        {
-            cwd: workingDirectory,
+    try {
+        await exec(
+            await getTestCommand(testCommand, 'report.json', workingDirectory),
+            [],
+            {
+                cwd: workingDirectory,
+            }
+        );
+    } catch (error) {
+        const errMessage = getErrorMessage(error) ?? '';
+
+        if (THRESHOLD_NOT_MET_REGEX.test(errMessage)) {
+            const newErr = toErrorWithMessage(error);
+            const [newMessage] =
+                errMessage.match(THRESHOLD_NOT_MET_REGEX) ?? [];
+
+            newErr.message = newMessage;
+
+            throw newErr;
         }
-    );
+
+        throw error;
+    }
 };
--- tests/stages/runTest.test.ts
+++ tests/stages/runTest.test.ts
@@ -1,6 +1,7 @@
 import { exec } from '@actions/exec';
 
 import { runTest } from '../../src/stages/runTest';
+import { toErrorWithMessage } from '../../src/utils/getErrorMessage';
 
 const clearMocks = () => {
     (exec as jest.Mock<any, any>).mockClear();
@@ -27,5 +28,32 @@ describe('runTest', () => {
         expect(exec).toBeCalledWith(expect.any(String), [], {
             cwd: 'custom cwd',
         });
+    });
+
+    it('should run test script and return threshold errors if there are any', async () => {
+        (exec as jest.Mock).mockRejectedValueOnce(`
+        PASS some/__tests__/other-test.test.js
+        PASS some/__tests__/test.test.js
+        Jest: "global" coverage threshold for statements (88%) not met: 79.68%
+        Jest: "./some/path" coverage threshold for statements (45%) not met: 44.91%
+        
+        Test Suites: 5 skipped, 10 passed, 10 of 10 total
+        Tests:       17 skipped, 1 todo, 1010 passed, 1010 total
+        Snapshots:   116 passed, 116 total
+        Time:        4.786 s
+        Ran all test suites.
+        `);
+
+        let errMessage = '';
+
+        try {
+            await runTest('npm run test');
+        } catch (e) {
+            errMessage = toErrorWithMessage(e).message;
+        }
+
+        expect(errMessage).toEqual(
+            'Jest: \\"global\\" coverage threshold for statements (88%) not met: 79.68%\\n        Jest: \\"./some/path\\" coverage threshold for statements (45%) not met: 44.91%\\'
+        );
     });
 });
