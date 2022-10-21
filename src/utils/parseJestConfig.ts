0d4091d234723ed0fb3cfeab1b409379309c3eee
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-10-21 18:29:59 +0300

    Used c12 for configuration loading

--- src/utils/parseJestConfig.ts
+++ src/utils/parseJestConfig.ts
@@ -1,26 +1,8 @@
-import { resolve } from 'path';
-
-import { parseConfigurations, PossibleConfiguration } from 'cfgn';
-
-// These constants are taken from "jest-config" module, file "constants.js"
-const JEST_CONFIG_BASE_NAME = 'jest.config';
-const JEST_CONFIG_EXT_ORDER = Object.freeze([
-    '.js',
-    '.ts',
-    '.mjs',
-    '.cjs',
-    '.json',
-]);
+import { loadConfig } from 'c12';
 
 export const parseJestConfig = (workingDirectory: string): Promise<unknown> => {
-    const possibleJestConfigs: PossibleConfiguration[] = JEST_CONFIG_EXT_ORDER.map(
-        (extension) => ({
-            path: resolve(
-                workingDirectory,
-                JEST_CONFIG_BASE_NAME.concat(extension)
-            ),
-        })
-    );
-
-    return parseConfigurations(possibleJestConfigs);
+    return loadConfig({
+        cwd: workingDirectory,
+        name: 'jest',
+    });
 };
