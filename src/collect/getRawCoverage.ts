9f72aafce8de74c48d400ada3d14ed39baa3a5fe
Quentin Hu <quentin.hu@thredup.com>
2023-01-19 16:22:04 +0100

    test

--- src/collect/getRawCoverage.ts
+++ src/collect/getRawCoverage.ts
@@ -18,7 +18,7 @@ export const getRawCoverage = async (
     | { success: false; failReason: FailReason.TESTS_FAILED; error?: Error }
 > => {
     if (branch) {
-        // NOTE: It is possible that the 'git fetch -all' command will fail due to different file permissions, so allow that to fail gracefully
+        // NOTE:  It is possible that the 'git fetch -all' command will fail due to different file permissions, so allow that to fail gracefully
         try {
             await exec(`git fetch --all --depth=1`);
         } catch (err) {
