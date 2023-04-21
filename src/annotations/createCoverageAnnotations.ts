aeb17dd17024c7d3aa4d93bc458a2afed67a8e66
Chow Loong Jin <hyperair@debian.org>
2023-04-21 10:34:30 +0800

    Deal with inverted location ranges in coverage annotations

--- src/annotations/createCoverageAnnotations.ts
+++ src/annotations/createCoverageAnnotations.ts
@@ -14,12 +14,12 @@ const getLocation = (
     start_column?: number;
     end_column?: number;
 } => ({
-    start_line: start.line,
-    end_line: end.line,
-    ...start.line === end.line && start.column !== null && end.column !== null
+    start_line: Math.min(start.line, end.line),
+    end_line: Math.max(end.line),
+    ...start.line === end.line && start.column != null && end.column != null
         ? {
-            start_column: start.column,
-            end_column: end.column,
+            start_column: Math.max(1, Math.min(start.column, end.column)),
+            end_column: Math.max(1, start.column, end.column),
         }
         : {},
 });
