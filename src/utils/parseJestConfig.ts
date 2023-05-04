eac3b0c6d9efe9ef85a59176f45db6764ea559e2
Anton Joy <anton.joy@corva.ai>
2023-05-04 18:29:24 +0300

    Load jest config from package.json if no dedicated config file found

--- src/utils/parseJestConfig.ts
+++ src/utils/parseJestConfig.ts
@@ -3,10 +3,18 @@ import { loadConfig } from 'c12';
 export const parseJestConfig = async (
     workingDirectory: string
 ): Promise<unknown> => {
-    const { config } = await loadConfig({
+    let { config } = await loadConfig({
         cwd: workingDirectory,
         name: 'jest',
     });
+
+    if (!config || Object.keys(config).length === 0) {
+        config = await loadConfig({
+            cwd: workingDirectory,
+            name: 'jest',
+            packageJson: true,
+        });
+    }
 
     return config;
 };
