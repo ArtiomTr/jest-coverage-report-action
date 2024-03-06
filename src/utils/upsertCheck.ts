84cfdffa6e72b205262e1f0105401c9681cfc974
sirse <artiom.tretjakovas2@gmail.com>
2024-03-06 20:14:34 +0200

    Do not fail if checks: read permission is missing

--- src/utils/upsertCheck.ts
+++ src/utils/upsertCheck.ts
@@ -6,18 +6,31 @@ export const upsertCheck = async (
     octokit: ReturnType<typeof getOctokit>,
     check: CreateCheckOptions
 ) => {
-    const checks = await octokit.rest.checks.listForRef({
-        owner: check.owner as string,
-        repo: check.repo as string,
-        ref: check.head_sha as string,
-        check_name: check.name as string,
-    });
+    let check_id: number | undefined;
 
-    if (checks.data.check_runs.length !== 1) {
+    try {
+        const checks = await octokit.rest.checks.listForRef({
+            owner: check.owner as string,
+            repo: check.repo as string,
+            ref: check.head_sha as string,
+            check_name: check.name as string,
+        });
+
+        if (checks.data.check_runs.length === 1) {
+            check_id = checks.data.check_runs[0].id;
+        }
+    } catch {
+        console.warn(
+            'Missing `checks: read` permission. It is required to prevent bug when manually rerunning action adds check' +
+                ' twice. You can safely ignore this, and enable permission only if you encounter duplicate checks.'
+        );
+    }
+
+    if (check_id === undefined) {
         await octokit.rest.checks.create(check);
     } else {
         await octokit.rest.checks.update({
-            check_run_id: checks.data.check_runs[0].id,
+            check_run_id: check_id,
             ...check,
             head_sha: undefined,
         });
