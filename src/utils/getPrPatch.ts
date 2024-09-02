316ad4b11401b805bf0465e50be4269391e8bd82
syossan27 <wisdom1027@gmail.com>
2024-09-02 21:14:47 +0900

    Add fallback for getPrPatch.ts

--- src/utils/getPrPatch.ts
+++ src/utils/getPrPatch.ts
@@ -1,18 +1,29 @@
 import { context, getOctokit } from '@actions/github';
-
 import { Options } from '../typings/Options';
+import { Endpoints } from "@octokit/types";
+
+type getPullsResponse = Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}"]["response"];
 
 export async function getPrPatch(
     octokit: ReturnType<typeof getOctokit>,
     options: Options
 ): Promise<string> {
-    const response: { data: string } = ((await octokit.rest.pulls.get({
+    const pullsResponse: getPullsResponse = ((await octokit.rest.pulls.get({
         ...context.repo,
         pull_number: options.pullRequest!.number,
+    })) as unknown) as getPullsResponse;
+
+    const headSha = pullsResponse.data.head.sha;
+    const baseSha = pullsResponse.data.base.sha;
+
+    const compareResponse: {data: string} = ((await octokit.rest.repos.compareCommits({
+        ...context.repo,
+        base: baseSha,
+        head: headSha,
         headers: {
             accept: 'application/vnd.github.v3.patch',
         },
-    })) as unknown) as { data: string };
-    const patch = response.data;
-    return patch;
+    }) as unknown) as {data: string});
+
+    return compareResponse.data;
 }
