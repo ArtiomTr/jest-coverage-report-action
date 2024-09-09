7f750dd50f5585533321eb7ebc482b936b49a5d4
sirse <artiom.tretjakovas2@gmail.com>
2024-09-09 22:07:29 +0300

    Fix linting issues

--- src/utils/getPrPatch.ts
+++ src/utils/getPrPatch.ts
@@ -1,8 +1,9 @@
 import { context, getOctokit } from '@actions/github';
-import { Options } from '../typings/Options';
-import { Endpoints } from "@octokit/types";
+import { Endpoints } from '@octokit/types';
 
-type getPullsResponse = Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}"]["response"];
+import { Options } from '../typings/Options';
+
+type getPullsResponse = Endpoints['GET /repos/{owner}/{repo}/pulls/{pull_number}']['response'];
 
 export async function getPrPatch(
     octokit: ReturnType<typeof getOctokit>,
@@ -16,14 +17,16 @@ export async function getPrPatch(
     const headSha = pullsResponse.data.head.sha;
     const baseSha = pullsResponse.data.base.sha;
 
-    const compareResponse: {data: string} = ((await octokit.rest.repos.compareCommits({
+    const compareResponse: {
+        data: string;
+    } = ((await octokit.rest.repos.compareCommits({
         ...context.repo,
         base: baseSha,
         head: headSha,
         headers: {
             accept: 'application/vnd.github.v3.patch',
         },
-    }) as unknown) as {data: string});
+    })) as unknown) as { data: string };
 
     return compareResponse.data;
 }
